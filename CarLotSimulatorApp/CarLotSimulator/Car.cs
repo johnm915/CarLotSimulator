4f272a61d0d0046686f8361053dec1ac43bb3306
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-01-25 13:43:07 -0600

    CarLot

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EnginNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveAble { get; set; }
+
+        
+        
+
+        
+        public Car(int year, string make, string model, string enginNoise, string honkNoise, bool isDriveAble = false)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EnginNoise = enginNoise;
+            HonkNoise = honkNoise;
+
+        }
+        
+        public void  MakeEnginNoise(string enginNoise)
+        {
+            Console.WriteLine(enginNoise);
+        }
+        public void MakeHonkNoise(string HonkNoise)
+        {
+           Console.WriteLine(HonkNoise);
+        }
+        
+
+
+
+
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,11 +18,50 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            Carlot lot = new Carlot();
+
+            Car toyota = new Car();
+            lot.CarList.Add(toyota);
+
+
+            
+            toyota.Year = 2015;
+            toyota.Make = "Toyota";
+            toyota.Model = "Corolla";
+            toyota.EnginNoise = "vroooom";
+            toyota.HonkNoise = "beep beep";
+            toyota.IsDriveAble = true;
+
+            
+            toyota.MakeEnginNoise(toyota.EnginNoise);
+            toyota.MakeHonkNoise(toyota.HonkNoise);
+
+            //object
+            Car Camery = new Car() { Year = 2020, Make = "Toyota", Model="Camery", EnginNoise= "vrom vrom", HonkNoise = "ting ting" };
+            Camery.MakeEnginNoise(Camery.EnginNoise);
+            Camery.MakeHonkNoise(Camery.HonkNoise);
+            lot.CarList.Add(Camery);
+
+            //constructor
+            Car Junk = new Car { Year = 1980, Make = "Junk", Model = "Black", EnginNoise = "nothing", HonkNoise = "loud" };
+            Junk.MakeEnginNoise(Junk.EnginNoise);
+            Junk.MakeHonkNoise(Junk.HonkNoise);
+
+            Console.WriteLine("---------------");
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
             //*************BONUS X 2*************//
+
+            foreach (var car in lot.CarList)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+            }
+
+
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
